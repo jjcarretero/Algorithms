2f29c07f73f5bfe875c59403ccbe4e40110c6a5e
JohnShakalaka <juanjosecarreteroa@hotmail.com>
2020-09-19 13:48:02 +0200

    v.1909

--- Cs/sequentialdigits.cs
+++ Cs/sequentialdigits.cs
@@ -19,6 +19,8 @@ public class Solution
 		{
 			if ((solutions[i] >= low) && (solutions[i] <= high))
 				ret.Add(solutions[i]);
+			else if (solutions[i] > high)
+				break ;
 			i++;
 		}
 		return (ret);
